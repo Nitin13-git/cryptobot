e5db53249fd22e8659fc857593f14ef86100c741
Nitin Kushwaha <79303072+Nitin13-git@users.noreply.github.com>
2022-01-27 23:41:09 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotAllFeatures.sln -p:DeployOnBuild=true -p:PublishProfile=.\eastus-bot1-Web-Deploy.pubxml -p:Password=l6mF7DWdGRu32SoBdSygKcLsjxMdz0zMq3Bkj7qmmZq6ogWX3FE2qSWunpwK
+
